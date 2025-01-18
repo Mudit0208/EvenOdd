3aa31eb57dfd06b71aff88ae9d5a49ec626810b3
Mudit0208 <mudit.jadonn@gmail.com>
2025-01-18 15:05:05 +0530

    first commit

---
+++ helloWorld.java
@@ -0,0 +1,18 @@
+public class helloWorld{
+public static void main(String[] args){
+System.out.println("Even nnumbers : ");
+for(int i = 1; i<=10; i++){
+if(i % 2 == 0){
+System.out.print(i + " ");
+}
+}
+System.out.println();
+System.out.println("Odd numbers :");
+for(int i=1; i<=10; i++){
+if(i % 2 != 0){
+System.out.print(i + " ");
+}
+}
+}
+
+}
